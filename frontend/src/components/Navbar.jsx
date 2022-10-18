09d1d940019b8f0516fd3e73b86bc1db78444d27
biancarosa99 <bianca_rosa99@yahoo.com>
2022-10-18 14:36:40 +0300

    made navbar responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "../styles/Navbar.css";
-import LogoIMG from '../assets/fithublogo2.png'
+import LogoIMG from "../assets/fithublogo2.png";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [modalMenuIsOpen, setModalMenuIsOpen] = useState(false);
+
+  const openModalHandler = () => {
+    setModalMenuIsOpen(true);
+  };
+
+  const closeModalHandler = () => {
+    setModalMenuIsOpen(false);
+  };
   return (
     <nav className="navbar">
       <div className="brand-logo">
@@ -10,14 +22,33 @@ const Navbar = () => {
           <img className="logo" alt="logo" src={LogoIMG} />
         </a>
       </div>
-      <div className="menu-items">
-        <ul>
+
+      <div className="menu-items-desktop">
+        <li className="menu-item">HOME</li>
+        <li className="menu-item">PLANS</li>
+        <li className="menu-item">LOGIN</li>
+        <li className="menu-item">REGISTER</li>
+      </div>
+
+      {!modalMenuIsOpen && (
+        <li onClick={openModalHandler} className="menu-button">
+          <MenuRoundedIcon />
+        </li>
+      )}
+
+      {modalMenuIsOpen && (
+        <li onClick={closeModalHandler} className="menu-button">
+          <CloseRoundedIcon />
+        </li>
+      )}
+      {modalMenuIsOpen && (
+        <div className="menu-items-mobile">
           <li className="menu-item">HOME</li>
           <li className="menu-item">PLANS</li>
           <li className="menu-item">LOGIN</li>
           <li className="menu-item">REGISTER</li>
-        </ul>
-      </div>
+        </div>
+      )}
     </nav>
   );
 };
