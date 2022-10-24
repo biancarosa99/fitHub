0ec196c65167e174aa5d55e02b38926b749834eb
biancarosa99 <bianca_rosa99@yahoo.com>
2022-10-24 18:33:23 +0300

    fixed problems with home info mobile component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
 import HomeInfoContainer from "./components/HomeInfoContainer";
-// import HomeInfoMobileContainer from "./components/HomeInfoMobileContainer";
+import HomeInfoMobileContainer from "./components/HomeInfoMobileContainer";
 import Navbar from "./components/Navbar";
 
 function App() {
-  // const isDesktop = useMediaQuery({ query: "(min-width: 451px)" });
-  // const isMobile = useMediaQuery({ query: "(max-width: 450px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width: 451px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 450px)" });
   return (
     <React.Fragment>
       <Navbar />
-      {/* {isDesktop && <HomeInfoContainer />}
-      {isMobile && <HomeInfoMobileContainer />} */}
-      <HomeInfoContainer />
+      {isDesktop && <HomeInfoContainer />}
+      {isMobile && <HomeInfoMobileContainer />}
+      {/* <HomeInfoContainer /> */}
     </React.Fragment>
   );
 }
--- frontend/src/components/HomeInfoMobileContainer.jsx
+++ frontend/src/components/HomeInfoMobileContainer.jsx
@@ -4,16 +4,15 @@ import LogoIMG from "../assets/women-fitness.jpg";
 
 const HomeInfoMobileContainer = () => {
   return (
-    <div className="info-container">
-      <div className="text-container">
-        <div></div>
-        <div className="title">SHAPE YOURSELF</div>
-        <div className="description">
-          Our club encourages wellness by providing first-class instructors,
-          innovative classes, and qualified staff.
-        </div>
-      </div>
+    <div className="info-mobile-container">
       <div className="wrapper">
+        <div className="text-container">
+          <div className="title">SHAPE YOURSELF</div>
+          <div className="description">
+            Our club encourages wellness by providing first-class instructors,
+            innovative classes, and qualified staff.
+          </div>
+        </div>
         <div className="image-container">
           <img className="fitness-img" src={LogoIMG} />
           {/* <button className="button">JOIN NOW</button> */}
