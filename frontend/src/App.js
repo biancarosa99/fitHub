161ddbc1bf5335160ffa6644d6e7e006d5570b64
biancarosa99 <79415140+biancarosa99@users.noreply.github.com>
2022-11-02 19:10:11 +0200

    Create home info component (#2)
    * added home info container component
    
    * fixed problems with home info mobile component
    
    * solved problems with home info container and navbar on mobile
    
    * added button on mobile home container and changed home container responsive styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
+import HomeInfoContainer from "./components/HomeInfoContainer";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const isDesktop = useMediaQuery({ query: "(min-width: 451px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 450px)" });
   return (
     <React.Fragment>
       <Navbar />
+      <HomeInfoContainer />
     </React.Fragment>
   );
 }
---
+++ frontend/src/components/HomeInfoContainer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "../styles/HomeInfoContainer.css";
+import fitnessIMG from "../assets/women-fitness.jpg";
+import { useMediaQuery } from "react-responsive";
+
+const HomeInfoContainer = () => {
+  const isDesktop = useMediaQuery({ query: "(min-width: 451px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 450px)" });
+  return (
+    <React.Fragment>
+      {isDesktop && (
+        <div className="info-container">
+          <div className="wrapper">
+            <div className="image-container">
+              <img className="fitness-img" src={fitnessIMG} />
+            </div>
+            <div className="text-container">
+              <div className="title">SHAPE YOURSELF</div>
+              <div className="description">
+                Our club encourages wellness by providing first-class
+                instructors, innovative classes, and qualified staff.
+              </div>
+              <button className="button">JOIN NOW</button>
+            </div>
+          </div>
+        </div>
+      )}
+      {isMobile && (
+        <div className="info-mobile-container">
+          <div className="wrapper">
+            <div className="text-container">
+              <div className="title">SHAPE YOURSELF</div>
+              <div className="description">
+                Our club encourages wellness by providing first-class
+                instructors, innovative classes, and qualified staff.
+              </div>
+              <button className="button">JOIN NOW</button>
+            </div>
+            <div className="image-container">
+              <img className="fitness-img" src={fitnessIMG} />
+            </div>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default HomeInfoContainer;
