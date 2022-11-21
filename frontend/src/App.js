ca05be59bac2806e854efd80bc509b212a9d8adb
BiancaRosa <79415140+biancarosa99@users.noreply.github.com>
2022-11-21 14:05:13 +0200

    Create login and register modals (#3)
    * added login modal component
    
    * added register component
    
    * added open and close logic for login and register modals for desktop and mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
 import HomeInfoContainer from "./components/HomeInfoContainer";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import Register from "./components/Register";
 
 function App() {
   const isDesktop = useMediaQuery({ query: "(min-width: 451px)" });
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,9 +4,13 @@ import LogoIMG from "../assets/fithublogo2.png";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
 
 const Navbar = () => {
   const [modalMenuIsOpen, setModalMenuIsOpen] = useState(false);
+  const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
+  const [registerModalIsOpen, setRegisterModalIsOpen] = useState(false);
 
   const openModalHandler = () => {
     setModalMenuIsOpen(true);
@@ -15,41 +19,74 @@ const Navbar = () => {
   const closeModalHandler = () => {
     setModalMenuIsOpen(false);
   };
+
+  const openLoginModalHandler = () => {
+    setLoginModalIsOpen(true);
+    modalMenuIsOpen && setModalMenuIsOpen(false);
+  };
+
+  const closeLoginModalHandler = () => {
+    setLoginModalIsOpen(false);
+  };
+
+  const openRegisterModalHandler = () => {
+    setRegisterModalIsOpen(true);
+    modalMenuIsOpen && setModalMenuIsOpen(false);
+  };
+
+  const closeRegisterModalHandler = () => {
+    setRegisterModalIsOpen(false);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="brand-logo">
-        <a href="/">
-          <img className="logo" alt="logo" src={LogoIMG} />
-        </a>
-      </div>
+    <React.Fragment>
+      <nav className="navbar">
+        <div className="brand-logo">
+          <a href="/">
+            <img className="logo" alt="logo" src={LogoIMG} />
+          </a>
+        </div>
 
-      <div className="menu-items-desktop">
-        <li className="menu-item">HOME</li>
-        <li className="menu-item">PLANS</li>
-        <li className="menu-item">LOGIN</li>
-        <li className="menu-item">REGISTER</li>
-      </div>
-
-      {!modalMenuIsOpen && (
-        <li onClick={openModalHandler} className="menu-button">
-          <MenuRoundedIcon />
-        </li>
-      )}
-
-      {modalMenuIsOpen && (
-        <li onClick={closeModalHandler} className="menu-button">
-          <CloseRoundedIcon />
-        </li>
-      )}
-      {modalMenuIsOpen && (
-        <div className="menu-items-mobile">
+        <div className="menu-items-desktop">
           <li className="menu-item">HOME</li>
           <li className="menu-item">PLANS</li>
-          <li className="menu-item">LOGIN</li>
-          <li className="menu-item">REGISTER</li>
+          <li className="menu-item" onClick={openLoginModalHandler}>
+            LOGIN
+          </li>
+          <li className="menu-item" onClick={openRegisterModalHandler}>
+            REGISTER
+          </li>
         </div>
+
+        {!modalMenuIsOpen && (
+          <li onClick={openModalHandler} className="menu-button">
+            <MenuRoundedIcon />
+          </li>
+        )}
+
+        {modalMenuIsOpen && (
+          <li onClick={closeModalHandler} className="menu-button">
+            <CloseRoundedIcon />
+          </li>
+        )}
+        {modalMenuIsOpen && (
+          <div className="menu-items-mobile">
+            <li className="menu-item">HOME</li>
+            <li className="menu-item">PLANS</li>
+            <li className="menu-item" onClick={openLoginModalHandler}>
+              LOGIN
+            </li>
+            <li className="menu-item" onClick={openRegisterModalHandler}>
+              REGISTER
+            </li>
+          </div>
+        )}
+      </nav>
+      {loginModalIsOpen && <Login closeLogin={closeLoginModalHandler} />}
+      {registerModalIsOpen && (
+        <Register closeRegister={closeRegisterModalHandler} />
       )}
-    </nav>
+    </React.Fragment>
   );
 };
 
