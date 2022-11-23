f80365d0b78b1a05bc099644f5c37553f100df4c
BiancaRosa <79415140+biancarosa99@users.noreply.github.com>
2022-11-24 00:04:23 +0200

    Create scheduler component (#6)
    * added fitness classes scheduler
    
    * removed scheduler component from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
 import FitnessClassesCarousel from "./components/FitnessClassesCarousel";
 import HomeInfoContainer from "./components/HomeInfoContainer";
-import Login from "./components/Login";
 import Navbar from "./components/Navbar";
-import Register from "./components/Register";
 
 function App() {
-  const isDesktop = useMediaQuery({ query: "(min-width: 451px)" });
-  const isMobile = useMediaQuery({ query: "(max-width: 450px)" });
   return (
     <React.Fragment>
       <Navbar />
