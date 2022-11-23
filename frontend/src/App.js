7b1d067d24248a977f23e68d3c09c61be707bfb1
biancarosa99 <bianca_rosa99@yahoo.com>
2022-11-24 00:01:35 +0200

    removed scheduler component from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
-import FitnessScheduler from "./components/FitnessScheduler";
 import HomeInfoContainer from "./components/HomeInfoContainer";
-import Login from "./components/Login";
 import Navbar from "./components/Navbar";
-import Register from "./components/Register";
 
 function App() {
-  const isDesktop = useMediaQuery({ query: "(min-width: 451px)" });
-  const isMobile = useMediaQuery({ query: "(max-width: 450px)" });
   return (
     <React.Fragment>
       <Navbar />
       <HomeInfoContainer />
-      <FitnessScheduler />
     </React.Fragment>
   );
 }
