802f1cbce4b22c19655c39fda5dbe856a7952d68
biancarosa99 <bianca_rosa99@yahoo.com>
2022-11-27 17:24:06 +0200

    connected user register form with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import FitnessClassesCarousel from "./components/FitnessClassesCarousel";
 import HomeInfoContainer from "./components/HomeInfoContainer";
 import Navbar from "./components/Navbar";
+import axios from "axios";
+
+// axios.defaults.baseURL = "http://localhost:3000";
 
 function App() {
   return (
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../styles/Register.module.css";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import axios from "axios";
+
 const Register = (props) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const userRegisterHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("auth/register", {
+        email,
+        password,
+        confirmPassword,
+        firstName,
+        lastName,
+      });
+      if (res.status === 200) {
+        console.log("User registered succesfully!");
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+      }
+    } catch (err) {
+      console.log("error");
+    }
+
+    console.log(firstName, lastName, email, password, confirmPassword);
+  };
+
   return (
     <React.Fragment>
       <div className={classes.backdrop}>
@@ -15,22 +49,54 @@ const Register = (props) => {
               <CloseRoundedIcon />
             </button>
           </div>
-          <form className={classes["form-section"]}>
+          <form
+            className={classes["form-section"]}
+            onSubmit={userRegisterHandler}
+          >
             <div className={classes["input-section"]}>
               <div className={classes["input-label"]}>Firstname</div>
-              <input type="text" className={classes.input} />
+              <input
+                type="text"
+                className={classes.input}
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </div>
             <div className={classes["input-section"]}>
               <div className={classes["input-label"]}>Lastname</div>
-              <input type="text" className={classes.input} />
+              <input
+                type="text"
+                className={classes.input}
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
             </div>
             <div className={classes["input-section"]}>
               <div className={classes["input-label"]}>Email</div>
-              <input type="email" className={classes.input} />
+              <input
+                type="email"
+                className={classes.input}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className={classes["input-section"]}>
               <div className={classes["input-label"]}>Password</div>
-              <input type="password" className={classes.input} />
+              <input
+                type="password"
+                className={classes.input}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className={classes["input-section"]}>
+              <div className={classes["input-label"]}> Confirm Password</div>
+              <input
+                type="password"
+                className={classes.input}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
             </div>
             <div className={classes["signup-section"]}>
               <span>Already have an account? </span>
