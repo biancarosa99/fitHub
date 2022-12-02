e86fdb7400e637598a52f50e8b0c3bab5d7953e5
biancarosa99 <bianca_rosa99@yahoo.com>
2022-12-02 15:31:04 +0200

    added fitness classes timetable page

---
+++ frontend/src/pages/SchedulerPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FitnessScheduler from "../components/FitnessScheduler";
+
+const SchedulerPage = () => {
+  return (
+    <React.Fragment>
+      <FitnessScheduler />
+    </React.Fragment>
+  );
+};
+
+export default SchedulerPage;
