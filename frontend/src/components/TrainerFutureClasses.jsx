1b9cb92f29fd53d45dc2ad0127c48d480ed1fd5c
biancarosa99 <bianca_rosa99@yahoo.com>
2022-12-08 19:49:09 +0200

    added pagination component for trainer classes table

--- frontend/src/components/TrainerFutureClasses.jsx
+++ frontend/src/components/TrainerFutureClasses.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "../styles/TrainerFutureClasses.css";
+import Pagination from "@mui/material/Pagination";
+import { useMediaQuery } from "react-responsive";
 
 const TrainerFutureClasses = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 548px)" });
+
+  const paginationComponentSize = isMobile ? "small" : "large";
   return (
     <div className="table-container">
       <table className="trainer-classes-table">
@@ -52,8 +57,50 @@ const TrainerFutureClasses = () => {
               </button>
             </td>
           </tr>
+
+          <tr>
+            <td data-label="Date" className="fitness-class-start-hour">
+              12/12/20 10:00
+            </td>
+            <td data-label="Class">TABATA FITNESS</td>
+            <td data-label="Location">FitHub1</td>
+            <td>
+              <button className="view-participants-button">
+                View Particiants
+              </button>
+            </td>
+          </tr>
+
+          <tr>
+            <td data-label="Date" className="fitness-class-start-hour">
+              12/12/20 10:00
+            </td>
+            <td data-label="Class">TABATA FITNESS</td>
+            <td data-label="Location">FitHub1</td>
+            <td>
+              <button className="view-participants-button">
+                View Particiants
+              </button>
+            </td>
+          </tr>
+
+          <tr>
+            <td data-label="Date" className="fitness-class-start-hour">
+              12/12/20 10:00
+            </td>
+            <td data-label="Class">TABATA FITNESS</td>
+            <td data-label="Location">FitHub1</td>
+            <td>
+              <button className="view-participants-button">
+                View Particiants
+              </button>
+            </td>
+          </tr>
         </tbody>
       </table>
+      <div className="pagination-container">
+        <Pagination count={10} size={paginationComponentSize} />
+      </div>
     </div>
   );
 };
