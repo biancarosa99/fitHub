4ab9bd08dde1956d298723a828d630dbe6226e27
biancarosa99 <bianca_rosa99@yahoo.com>
2025-02-11 17:41:59 +0200

    include verifyToken middleware

--- backend/src/controllers/TrainerController.ts
+++ backend/src/controllers/TrainerController.ts
@@ -13,6 +13,6 @@ router.post("/trainer/create", verifyToken, createFitnessClass); // create a sch
 router.delete("/trainer/:id", verifyToken, removeFitnessClass); // remove a scheduled class
 router.get("/trainer/future", verifyToken, getTrainerClasses); // get all the upcoming trainer's classes
 router.get("/trainer/past", verifyToken, getPastTrainerClasses); // get all the past trainer's classes
-router.post("/trainer/create-meeting", createMeeting);
+router.post("/trainer/create-meeting", verifyToken, createMeeting);
 
 module.exports = router;
